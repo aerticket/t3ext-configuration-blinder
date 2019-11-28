3bd9eb1a2b0f7d4c18a4079c7b2453fee305c39d
Thomas Heilmann <theilmann@aer.de>
2019-11-28 19:04:31 +0100

    Add key based configuration blinder

---
+++ Classes/Backend/KeyBasedConfigurationBlinder.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types=1);
+
+namespace Aerticket\ConfigurationBlinder\Backend;
+
+use TYPO3\CMS\Lowlevel\Controller\ConfigurationController;
+
+/**
+ * This class allows to blind confidential configuration values by key. Just add the keys that should be blinded
+ * to the $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['configuration_blinder']['keysToBlind'] array. The class will search
+ * the EXTCONF array recursively for values of type string with this key.
+ */
+class KeyBasedConfigurationBlinder
+{
+    /**
+     * @param array $blindedConfigurationOptions
+     * @param ConfigurationController $configurationController
+     * @return array
+     */
+    public function modifyBlindedConfigurationOptions(
+        array $blindedConfigurationOptions,
+        ConfigurationController $configurationController
+    ) {
+        if (empty($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['configuration_blinder']['keysToBlind'])) {
+            return $blindedConfigurationOptions;
+        }
+
+        // Filter the EXTCONF array so that includes only values that should be blinded
+        $filteredArray = self::filterArrayRecursive($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']);
+        if (empty($filteredArray)) {
+            return $blindedConfigurationOptions;
+        }
+
+        // Actually blind the remaining values in the array
+        array_walk_recursive($filteredArray, function(&$value) {
+            $value = '******';
+        });
+
+        if (!is_array($blindedConfigurationOptions['TYPO3_CONF_VARS']['EXTCONF'])) {
+            $blindedConfigurationOptions['TYPO3_CONF_VARS']['EXTCONF'] = [];
+        }
+
+        $blindedConfigurationOptions['TYPO3_CONF_VARS']['EXTCONF'] = array_replace_recursive(
+            $filteredArray,
+            $blindedConfigurationOptions['TYPO3_CONF_VARS']['EXTCONF']
+        );
+
+        return $blindedConfigurationOptions;
+    }
+
+    /**
+     * Only keeps non empty arrays or string values to be blinded
+     *
+     * @param $input
+     * @return array
+     */
+    public static function filterArrayRecursive($input)
+    {
+        foreach ($input as &$value) {
+            if (is_array($value)) {
+                $value = self::filterArrayRecursive($value);
+            }
+        }
+        return array_filter($input, function($value, $key) {
+            $isNotEmptyArray = is_array($value) && !empty($value) && $key != 'configuration_blinder';
+            $isStringValueWithKeyToBlind = is_string($value)
+                && is_string($key)
+                && in_array($key, $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['configuration_blinder']['keysToBlind']);
+            return $isNotEmptyArray || $isStringValueWithKeyToBlind;
+        }, ARRAY_FILTER_USE_BOTH);
+    }
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -14,9 +14,14 @@ if (!defined('TYPO3_MODE')) {
     }
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][$className]['modifyBlindedConfigurationOptions'][] =
         \Aerticket\ConfigurationBlinder\Backend\SelectiveConfigurationBlinder::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][$className]['modifyBlindedConfigurationOptions'][] =
+        \Aerticket\ConfigurationBlinder\Backend\KeyBasedConfigurationBlinder::class;
 
     if (!is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['configuration_blinder']['blindedConfigurationOptions'])) {
         $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['configuration_blinder']['blindedConfigurationOptions'] = [];
+    }
+    if (!is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['configuration_blinder']['keysToBlind'])) {
+        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['configuration_blinder']['keysToBlind'] = [];
     }
 })();
 
